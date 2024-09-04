5cfda8a36c593a4a89fe2ea2c7f6403a81241537
Eyad Hailat <Eyad.Hailat@gmail.com>
2024-09-04 05:40:53 -0400

    Formatting files.

--- server/djangoapp/microservices/sentiment/readme.md
+++ server/djangoapp/microservices/sentiment/readme.md
@@ -1,1 +1,2 @@
-The zip file in this directory is going to be used for Sentiment Analysis. You can find more information about this in this [link](https://www.nltk.org/data.html)
+The zip file in this directory is going to be used for Sentiment Analysis. You can find more information about this in
+this [link](https://www.nltk.org/data.html)
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,7 @@
 # Uncomment the imports below before you add the function code
-import requests
 import os
+
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,11 +16,11 @@ sentiment_analyzer_url = os.getenv(
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
@@ -30,9 +31,10 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred")
 
+
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
-    
+    request_url = sentiment_analyzer_url + "analyze/" + text
+
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -43,12 +45,12 @@ def analyze_review_sentiments(text):
 
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
         print("Network exception occurred")
-        
+
 # Add code for posting review
