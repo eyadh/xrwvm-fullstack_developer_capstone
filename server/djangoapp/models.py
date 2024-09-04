ad58205a6baf099ddcfadaa62c97ab8c30aeb23d
Eyad Hailat <Eyad.Hailat@gmail.com>
2024-09-04 05:36:47 -0400

    Removing an unused import. Formatting files.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,12 @@
 # Uncomment the following imports before adding the Model code
 
-from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 
 # Create your models here.
 
-class CarMake(models.Model):    
+class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
@@ -27,12 +26,12 @@ class CarModel(models.Model):
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPE, default= 'SUV')
-    year = models.IntegerField(default=2023, 
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
-    
+    type = models.CharField(max_length=10, choices=CAR_TYPE, default='SUV')
+    year = models.IntegerField(default=2023,
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
+
     def __str__(self):
-        return self.name 
+        return self.name
